d5ffb6872208aa39b2b8581b841dad6a9fbb6bd0
prem096 <ithampremchand7@gmail.com>
2025-06-26 18:25:24 +0530

    Added scikit-learn to requirements

--- app.py
+++ app.py
@@ -220,7 +220,6 @@ def download_all_pdfs():
         download_name='all_summaries.zip'
     )
 
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000)) 
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
